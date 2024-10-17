0349c044a331227dd98fe90cbd2ac85a9d60a72f
rosenclever <rosenclever@gmail.com>
2024-10-16 21:07:47 -0300

    sobrecarga de construtor de cliente

--- ControleFinanceiro/Model/Cliente.cs
+++ ControleFinanceiro/Model/Cliente.cs
@@ -11,11 +11,18 @@ namespace ControleFinanceiro.Model
         public string Nome {  get;  set; }
         public string Cpf { get; set; }
         public List<Conta> Contas { get; set; }
+        public string Telefone { get; set; }
         public Cliente(string nome, string cpf)
         {
             Nome = nome;
             Cpf = cpf;
         }
+
+        public Cliente(string nome, string cpf, string telefone) : this(nome, cpf)
+        {
+            Telefone = telefone;
+        }
+
         public override string ToString()
         {
             return Nome + " - " + Cpf;
