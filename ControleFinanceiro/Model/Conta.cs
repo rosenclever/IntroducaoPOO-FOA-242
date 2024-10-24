fd4572dbceeff39b4012cc3df640bd7504c7560e
rosenclever <rosenclever@gmail.com>
2024-10-23 21:06:26 -0300

    classe abstrata conta, saldo protegido, subclasse contaespecial

--- ControleFinanceiro/Model/Conta.cs
+++ ControleFinanceiro/Model/Conta.cs
@@ -6,20 +6,13 @@ using System.Threading.Tasks;
 
 namespace ControleFinanceiro.Model
 {
-    public class Conta
+    public abstract class Conta
     {
         public long Numero { get; set; }
-        public decimal Saldo { get; private set; }
+        public decimal Saldo { get; protected set; }
         public Cliente Titular { get; set; }
-        public void Saque(decimal valor)
-        {
-            if(valor <= Saldo && valor > 0)
-                Saldo -= valor;
-        }
-        public void Deposito(decimal valor)
-        {
-            if(valor > 0)
-                Saldo += valor;
-        }
+        public abstract void Saque(decimal valor);
+        public abstract void Deposito(decimal valor);
+        public abstract void Transferencia(Conta destino, decimal valor);
     }
 }
---
+++ ControleFinanceiro/Model/ContaEspecial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleFinanceiro.Model
+{
+    public class ContaEspecial : Conta
+    {
+        public decimal Limite { get; set; }
+        public override void Deposito(decimal valor)
+        {
+            throw new NotImplementedException();
+        }
+
+        //o saldo da conta nunca poderá ficar negativo além do seu limite
+        public override void Saque(decimal valor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Transferencia(Conta destino, decimal valor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
